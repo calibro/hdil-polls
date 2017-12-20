28a40dbe347e29f6a0cf73806527d20e9d8f3934
giorgio <giorgio@calib.ro>
2017-12-21 00:49:47 +0100

    change content home

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -9,6 +9,6 @@
  */
 angular.module('hdilPollsApp')
   .controller('HomeCtrl', function ($scope) {
-    $scope.title = "Human Data Interaction in Lombardia: Sviluppo di servizi di visualizzazione interattiva per il pubblico e l’amministratore";
-    $scope.description = "Al link sottostante è possibile accedere ad un servizio che permette di visualizzare, per un certo insieme di datasets presenti sul portale Open Data della Regione Lombardia, il loro punteggio di engagement (coinvolgimento), ovvero un indice del livello di interazione di un utente con il portale Open Data di Regione Lombardia. Tale punteggio si può calcolare a livello di singolo dataset, di categoria e di diversa granularità temporale. Per ulteriori informazioni (anche di natura metodologica) è possibile contattare il coordinatore del progetto, il Dott. Ing. Federico Cabitza (cabitza@disco.unimib.it)";
+    $scope.title = "Valore sociale della visualizzazione di open data di Regione Lombardia";
+    $scope.description = "Al link sottostante è possibile accedere ad un servizio che permette di visualizzare, per un certo insieme di infografiche, la loro qualità percepita, lungo le dimensioni di bellezza, informatività, intuitività e chiarezza, sulla base delle percezioni raccolte in uno studio multi-utente condotto all'interno delle attività di ricerca del progetto 'Il Valore Sociale degli Open Data' promosso da Regione Lombardia. Per ulteriori informazioni (anche di natura metodologica) è possibile contattare il coordinatore del progetto, il Dott. Ing. Federico Cabitza (cabitza @ disco.unimib.it)";
   });
