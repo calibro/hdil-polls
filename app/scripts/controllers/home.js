bc3fa2f9b6a9b9d316f2e743e4697e0536df8433
giorgio <giorgio@calib.ro>
2017-12-20 23:52:25 +0100

    added home description

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -9,6 +9,6 @@
  */
 angular.module('hdilPollsApp')
   .controller('HomeCtrl', function ($scope) {
-    $scope.title = "Human data interaction<br>in Lombardia";
-    $scope.description = "Lorem ipsum dolor sit amet, vim laudem accommodare te. Mea an constituto honestatis referrentur, eros philosophia vim ei. Qui in esse oportere, etiam postea quaestio te eos. Quo ut cibo ludus iracundia, ea iudico maiorum invenire eum. Vocent voluptatum at vel, et modus ignota commune has, dico splendide ut mel. In eripuit recusabo eum.";
+    $scope.title = "Human Data Interaction in Lombardia: Sviluppo di servizi di visualizzazione interattiva per il pubblico e l’amministratore";
+    $scope.description = "Al link sottostante è possibile accedere ad un servizio che permette di visualizzare, per un certo insieme di datasets presenti sul portale Open Data della Regione Lombardia, il loro punteggio di engagement (coinvolgimento), ovvero un indice del livello di interazione di un utente con il portale Open Data di Regione Lombardia. Tale punteggio si può calcolare a livello di singolo dataset, di categoria e di diversa granularità temporale. Per ulteriori informazioni (anche di natura metodologica) è possibile contattare il coordinatore del progetto, il Dott. Ing. Federico Cabitza (cabitza@disco.unimib.it)";
   });
